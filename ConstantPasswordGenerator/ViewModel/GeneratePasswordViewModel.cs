9d7d5807a507390a405a94b1c4cc024e15709ed6
actinic <sushil.pyakurel31@gmail.com>
2021-06-12 16:49:32 +0545

    app initial

---
+++ ConstantPasswordGenerator/ViewModel/GeneratePasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConstantPasswordGenerator.ViewModel
+{
+    public class GeneratePasswordViewModel
+    {
+        [Required]
+        public string Domain { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Range { get; set; } = "5:10";
+
+        public string Step { get; set; } = "1";
+    }
+}
